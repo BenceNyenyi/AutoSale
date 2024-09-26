e1992428ae96b4b81e8db9fed8ae3dfd560fa758
BenceNyenyi <nyenyestyanb@kkszki.hu>
2024-09-26 11:59:04 +0200

    Project is ready to use

--- Program.cs
+++ Program.cs
@@ -1,16 +1,106 @@
-﻿using System;
+﻿using AutoSale.Model;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AutoSale
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static connect conn = new connect();
+        public static List<car> lista = new List<car>();
+
+        public static void feladat1()
         {
 
+            string sql = "SELECT * FROM cars";
+            conn.Connection.Open();
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+            MySqlDataReader dr = cmd.ExecuteReader();   
+
+            dr.Read();
+
+            car cars = new car();
+
+            cars.id = dr.GetInt32(0);
+            cars.Brand = dr.GetString(1);
+            cars.Type = dr.GetString(2); 
+            cars.License = dr.GetString(3);
+            cars.Date = dr.GetString(4);
+
+            cars.Add(cars);
+            while (dr.Read()) ;
+           
+
+            conn.Connection.Close();
+            
+
+
+
+
+            conn.Connection.Close();
         }
+
+        static void Main(string[] args)
+        {
+            feladat1();
+            foreach (var item in lista)
+            {
+                Console.WriteLine($"Márka: {item.Brand}, Azonosító: {item.License}");
+            }
+        }
+
+
+        static void feladat2()
+        {
+            string marka, típus, azon;
+            int ev;
+
+
+
+            Console.WriteLine("Kérem az autó márkáját: ");
+            marka = Console.ReadLine();
+
+
+            Console.WriteLine("Kérem az autó márkáját: ");
+            típus =Console.ReadLine();
+
+
+            Console.WriteLine("Kérem az autó márkáját: ");
+            azon = Console.ReadLine();
+
+            Console.WriteLine("Kérem az autó márkáját: ");
+            ev = int.Parse(Console.ReadLine());
+
+            string sql = ("INSERT INTO");
+            conn.Connection.Open();
+
+           
+
+            
+
+            conn.Connection.Close();
+        }
+
+
+        public static void feladat3()
+        {
+            int id, ev;
+
+            Console.WriteLine("Kérem az autó id-ját: ");
+            id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Kérem az autó gyártás idejét: ");
+            id = int.Parse(Console.ReadLine());
+
+            string sql = ("UPDATE");
+        }
+
+       
     }
 }
---
+++ car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSale.Model
+{
+    public class car
+    {
+        public int id {get; set; }
+        public string Brand { get; set; }
+
+        public string Type { get; set; }
+
+        public string License { get; set; }
+        public string Date { get; set; }
+
+        internal void Add(car car)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
